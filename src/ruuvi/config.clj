61aef6681a097542dd8e74cf66c07572c196201c
Juha Syrjälä <juha.syrjala@gmail.com>
2015-09-19 19:49:33 +0300

    Improve docs for config.clj

--- src/ruuvi/config.clj
+++ src/ruuvi/config.clj
@@ -1,19 +1,18 @@
 (ns ruuvi.config
+  "Reading configuration files"
   (:require
    [clojure.edn :as edn]
-   )
-  )
+   ))
 
-(defn- validate [config]
-  config
-  )
+(defn- validate
+  "Validate configuration file. Check that all references files are found, all required data is found etc."
+  [config]
+  ;; TODO implement, use Schema to validate structure...
+  config)
 
-(defn read-config [file]
+(defn read-config
+  "Read and validates configuration file."
+  [file]
   (with-open [r (java.io.PushbackReader.
                  (clojure.java.io/reader file))]
     (validate (doall (edn/read r)))))
-
-
-;;(read-config "dev/config.edn")
-
-
